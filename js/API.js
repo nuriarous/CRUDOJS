c5a05d8f38f3d23288d2c2d8e5cb48b3f031528d
patofran <patofran69@gmail.com>
2024-01-09 17:43:03 +0100

    primeros pasos

--- js/API.js
+++ js/API.js
@@ -1,1 +1,18 @@
 //Acciones con la API de Json Server
+
+async function obtenerDatos(url) {
+    console.log(url);
+    try {
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+        return resultado;
+
+    } catch (err) {
+        console.log('obtenerDatos err', err.message);
+    }
+}
+
+export{
+    obtenerDatos,
+}
--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 //inicia la aplicación. Puede contener alguna función complementaria
+import { 
+    obtenerDatos,
+ } from "./API.js";
+
+ import { 
+    mostrarClientes,
+  } from "./funciones.js";
+
+//primero mostramos los clientes por defecto que tenemos en la pagina
+const url = "http://localhost:4000/clientes";
+const clientes = obtenerDatos(url);
+ 
+clientes.then( cli => {
+    mostrarClientes(cli);
+});
+
+window.onload = mostrarClientes();
