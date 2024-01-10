505a5e8dd0fc0040d3d906d51b43c8e1edf73fdb
nuriarous <nuria.romeroserrano@hotmail.com>
2024-01-10 16:08:09 +0100

    cliente
    no hace nada, ole

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -1,1 +1,27 @@
 //todo lo relacionado con la validación y llamada a función conexión API
+let formulario = document.getElementById('formulario');
+let botonAgregar = document.querySelector('.bg-teal-600');
+botonAgregar.addEventListener('click', nuevoCliente)
+
+function nuevoCliente() {
+
+    console.log('Hola');
+    /*
+    let nombre = document.getElementById('nombre').value;
+    let email = document.getElementById('email').value;
+    let telefono = document.getElementById('telefono').value;
+    let empresa = document.getElementById('empresa').value;
+
+    if (nombre == "" || email == "" || telefono == "" || empresa == "") {
+        let aviso = document.createElement('div');
+        let texto = document.createElement('p');
+        aviso.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded',
+            'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+        texto.innerHTML = '<strong>Error!</strong> Todos los campos son obligatorios';
+        aviso.appendChild(texto);
+        formulario.appendChild(aviso);
+    }
+*/
+
+
+}
