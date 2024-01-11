44e6d1642363b43d7ca52db5282978f8b79dea7b
patofran <patofran69@gmail.com>
2024-01-11 15:48:47 +0100

    subo

--- js/funciones.js
+++ js/funciones.js
@@ -7,6 +7,38 @@ function mostrarClientes(clientes) {
     });
 }
 
+function nuevoCliente() {
+    let nombre = document.getElementById('nombre').value;
+    let email = document.getElementById('email').value;
+    let telefono = document.getElementById('telefono').value;
+    let empresa = document.getElementById('empresa').value;
+
+    if (!avisoMostrado && (nombre == "" || email == "" || telefono == "" || empresa == "")) {
+        let aviso = document.createElement('div');
+        let texto = document.createElement('p');
+        aviso.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded',
+            'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+        texto.innerHTML = '<strong>Error!</strong> Todos los campos son obligatorios';
+        aviso.appendChild(texto);
+        formulario.appendChild(aviso);
+
+        setTimeout(function () {
+            formulario.removeChild(aviso);
+            avisoMostrado = false;
+        }, 5000);
+        avisoMostrado = true;
+    } else {
+        nombre.textContent = "nombre: " + nombre;
+        email.textContent = "email: " + email;
+        telefono.textContent = "telefono: " + telefono;
+        empresa.textContent = "empresa: " + empresa;
+        console.log(nombre, email, telefono, empresa);
+    }
+
+
+}
+
 export {
     mostrarClientes,
+    nuevoCliente
 }
--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -1,5 +1,10 @@
 //todo lo relacionado con la validación y llamada a función conexión API
 
+import { 
+    nuevoCliente
+ } from "./funciones.js";
+
+
 let formulario = document.getElementById('formulario');
 let botonAgregar = formulario.querySelector('.bg-teal-600');
 let avisoMostrado = false;
@@ -9,33 +14,4 @@ botonAgregar.addEventListener('click', function (e) {
     nuevoCliente();
 })
 
-function nuevoCliente() {
-    let nombre = document.getElementById('nombre').value;
-    let email = document.getElementById('email').value;
-    let telefono = document.getElementById('telefono').value;
-    let empresa = document.getElementById('empresa').value;
 
-    if (!avisoMostrado && (nombre == "" || email == "" || telefono == "" || empresa == "")) {
-        let aviso = document.createElement('div');
-        let texto = document.createElement('p');
-        aviso.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded',
-            'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
-        texto.innerHTML = '<strong>Error!</strong> Todos los campos son obligatorios';
-        aviso.appendChild(texto);
-        formulario.appendChild(aviso);
-
-        setTimeout(function () {
-            formulario.removeChild(aviso);
-            avisoMostrado = false;
-        }, 5000);
-        avisoMostrado = true;
-    } else {
-        nombre.textContent = "nombre: " + nombre;
-        email.textContent = "email: " + email;
-        telefono.textContent = "telefono: " + telefono;
-        empresa.textContent = "empresa: " + empresa;
-        console.log(nombre, email, telefono, empresa);
-    }
-
-
-}
